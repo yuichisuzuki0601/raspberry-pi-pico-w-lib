f6d055016264256b94e625fcc448c4946edbdd5b
yuichi <ys.development0601@gmail.com>
2025-11-11 22:23:16 +0900

    commit

--- src/lib/board/http_client.py
+++ src/lib/board/http_client.py
@@ -11,17 +11,33 @@ class HttpClient:
 
     def __noop(self, _): pass
 
-    # TODO body の型
-    def post(self, path: str, body, on_success, on_error = __noop):
+    def post(self, path: str, headers: dict, body: dict, on_success, on_error = __noop):
         print(f'POST {self.base_url}/{path} {jstNow()}')
         try:
+            m_headers = dict(headers or {})
+            m_headers['Content-Type'] = 'application/json'
+            m_headers['User-Agent'] = 'raspberry-pi-pico'
             response = urequests.post(
                 f'{self.base_url}/{path}',
                 data = dumps(body).encode('utf-8'),
-                headers = {
-                    'Content-Type': 'application/json',
-                    'User-Agent': 'raspberry-pi-pico'
-                },
+                headers = m_headers,
+                timeout = HttpClient.TIMEOUT
+            )
+            on_success(response)
+            response.close()
+        except Exception as e:
+            on_error(e)
+
+    def put(self, path: str, headers: dict, body: dict, on_success, on_error = __noop):
+        print(f'PUT {self.base_url}/{path} {jstNow()}')
+        try:
+            m_headers = dict(headers or {})
+            m_headers['Content-Type'] = 'application/json'
+            m_headers['User-Agent'] = 'raspberry-pi-pico'
+            response = urequests.put(
+                f'{self.base_url}/{path}',
+                data = dumps(body).encode('utf-8'),
+                headers = m_headers,
                 timeout = HttpClient.TIMEOUT
             )
             on_success(response)
